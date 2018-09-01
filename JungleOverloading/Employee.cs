67a5ea142950a20a1fc110c8a55be5b9e8df550a
Jeffrey Chen <jeffreychen2016@gmail.com>
2018-09-01 09:50:28 -0500

    finished JungleOverloading exercise

---
+++ JungleOverloading/Employee.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JungleOverloading
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public readonly List<string> Restaurants = new List<string>(new[] { "R1", "R2", "R3" });
+
+        public Employee(string firstName, string lastName)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+
+        public void eat()
+        {
+            var randomIndex = new Random();
+            Console.WriteLine($"{FirstName} {LastName} is eating at {Restaurants[randomIndex.Next(0,Restaurants.Count)]} restaurant");
+        }
+
+        public void eat(string food)
+        {
+            Console.WriteLine($"{FirstName} {LastName} is eating {food} at office");
+        }
+
+        public void eat(List<string> companions)
+        {
+            var randomIndex = new Random();
+            var randomRestaurant = Restaurants[randomIndex.Next(0, Restaurants.Count)];
+            var output = $"{this.FirstName} {this.LastName} is eating at {randomRestaurant} with ";
+            foreach (var companion in companions)
+            {
+                output += companion + " ";
+            }
+
+            Console.WriteLine(output);
+        }
+
+
+        public void eat(string food, List<string> companions)
+        {
+            var randomIndex = new Random();
+            var randomRestaurant = Restaurants[randomIndex.Next(0, Restaurants.Count)];
+            var output = $"{this.FirstName} {this.LastName} is eating {food} at {randomRestaurant} with ";
+            foreach (var companion in companions)
+            {
+                output += companion + " ";
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
--- JungleOverloading/Program.cs
+++ JungleOverloading/Program.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿using System.Collections.Generic;
 
 namespace JungleOverloading
 {
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var Jeff = new Employee("Jeffrey", "Chen");
+            var John = new Employee("John", "xxx");
+            var Ed = new Employee("Ed","aaa");
+            List<string> allCompanions = new List<string>();
+
+            allCompanions.Add(John.FirstName);
+            allCompanions.Add(Ed.FirstName);
+
+            Jeff.eat();
+            Jeff.eat("Tacobell");
+            Jeff.eat(allCompanions);
+            Jeff.eat("Tacobell",allCompanions);
         }
     }
 }
